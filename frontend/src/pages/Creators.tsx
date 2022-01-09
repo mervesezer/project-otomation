34523fe46223aefb9f8060b30807326849d05778
Artun Çolak <artuncolak97@gmail.com>
2022-01-09 16:24:34 +0300

    Creators degistirildi

--- frontend/src/pages/Creators.tsx
+++ frontend/src/pages/Creators.tsx
@@ -1,12 +1,41 @@
-import React from "react";
 import CreatorCard from "../components/ui/CreatorCard";
 
-export default function () {
+const creatorsData = [
+  {
+    email: "l1821012020@ogr.sdu.edu.tr",
+    image: "/merve.jpg",
+    location: "Edirne",
+    name: "Artun Çolak",
+    no: "1821012020",
+  },
+  {
+    email: "l1821012012@ogr.sdu.edu.tr",
+    image: "/merve.jpg",
+    location: "Edirne",
+    name: "Merve Sezer",
+    no: "1821012012",
+  },
+  {
+    email: "l1821012010@ogr.sdu.edu.tr",
+    image: "/merve.jpg",
+    location: "Demre",
+    name: "Osman Avcı",
+    no: "1821012010",
+  },
+];
+
+export default function Creators() {
   return (
-    <div className="flex">
-      <CreatorCard />
-      <CreatorCard />
-      <CreatorCard />
+    <div className="flex justify-around items-center h-full">
+      {creatorsData.map(({ email, image, location, name, no }) => (
+        <CreatorCard
+          email={email}
+          image={image}
+          location={location}
+          name={name}
+          no={no}
+        />
+      ))}
     </div>
   );
 }
--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,29 +1,78 @@
+import { useState } from "react";
+import Button from "../components/ui/Button";
+import Input from "../components/ui/Input";
 import { useAuth } from "../context/AuthContext";
-import Settings from "./Settings";
-
 
 export default function Profile() {
-    const { authUser } = useAuth();
-    return (
-        <div>
+  const { authUser } = useAuth();
+  const [name, setName] = useState(authUser.name);
+  const [lastName, setLastName] = useState(authUser.lastName);
+  const [email, setEmail] = useState(authUser.email);
+  const [password, setPassword] = useState("");
 
-            <div className="max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
-                <img
-                    className="object-cover mx-2 rounded-full h-9 w-9"
-                    src={`https://ui-avatars.com/api/?name=${authUser.name}+${authUser.lastName}`}
-                    alt="avatar"
-                />
+  return (
+    <div>
+      <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md">
+        <img
+          className="mx-auto w-52 mb-5"
+          src={`https://ui-avatars.com/api/?name=${authUser.name}+${authUser.lastName}&rounded=true&size=512`}
+          alt="avatar"
+        />
+        <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
+          Profil Ayarları
+        </h2>
 
-                <div className="px-6 py-4">
-                    <h1 className="text-xl font-semibold text-gray-800 dark:text-white">{authUser.name} {authUser.lastName}</h1>
-
-                    <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
-                        <h1 className="px-2 text-sm">{authUser.email}</h1>
-                    </div>
-                </div>
+        <form>
+          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2 ">
+            <div>
+              <label className="text-gray-700 dark:text-gray-200">Ad</label>
+              <Input
+                id="username"
+                value={name}
+                type="text"
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
-            <Settings/>
 
-        </div>
-    )
+            <div>
+              <label className="text-gray-700 dark:text-gray-200">Soyad</label>
+              <Input
+                id="soyad"
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label className="text-gray-700 dark:text-gray-200">Email</label>
+              <Input
+                id="email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label className="text-gray-700 dark:text-gray-200">
+                Yeni Şifre
+              </label>
+              <Input id="password" type="password" />
+            </div>
+
+            <div>
+              <label className="text-gray-700 dark:text-gray-200">
+                Yeni Şifre Tekrar
+              </label>
+              <Input id="passwordConfirmation" type="password" />
+            </div>
+          </div>
+
+          <div className="flex justify-end mt-6">
+            <Button text="Güncelle" />
+          </div>
+        </form>
+      </section>
+    </div>
+  );
 }
